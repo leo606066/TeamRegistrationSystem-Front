6f191ed7340dba928c0f7d11ef29bad22df32b24
leo606066 <239599191@qq.com>
2023-10-08 15:18:29 +0800

    Leo

---
+++ src/components/profile.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <el-card>
+    <template #header>
+      <span>关于我们</span>
+    </template>
+    布吉岛
+  </el-card>
+  <el-card>
+    
+  </el-card>
+</template>
+
+<style scoped>
+  .el-card {
+    width: 480px;
+    margin-bottom: 10px;
+  }
+  .el-card:last-child {
+    margin-bottom: 0;
+  }
+</style>

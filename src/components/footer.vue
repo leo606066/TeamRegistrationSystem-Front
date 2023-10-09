ff7bfd6e8e365ab5ebc58dae56cf33a877c95fe5
HerveyB3B4 <zhwaaaaa@outlook.com>
2023-10-09 16:00:43 +0800

    Footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -5,10 +5,10 @@
 <template>
   <el-divider content-position="center">@团队报名系统 2023.10</el-divider>
   <strong><el-text>点击焦♂流♂前端＆后端</el-text><br></strong>
-<el-link href="https://github.com/HerveyB3B4/TeamRegistrationSystem-Front">前端hvのgit仓库</el-link>
-&nbsp;
-&nbsp;
-<el-link href="https://github.com/Penryn/TeamRegistrationSystem-Back">后端phlのgit仓库</el-link>
+  <el-link href="https://github.com/HerveyB3B4/TeamRegistrationSystem-Front">前端hvのgit仓库</el-link>
+  &nbsp;
+  &nbsp;
+  <el-link href="https://github.com/Penryn/TeamRegistrationSystem-Back">后端phlのgit仓库</el-link>
 
 </template>
 

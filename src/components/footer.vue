0b8ab75f92ea792684ca205981e7e70eacd1030d
leo606066 <239599191@qq.com>
2023-10-09 15:22:40 +0800

    Leo

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,23 +3,6 @@
 </script>
 
 <template>
-<<<<<<< HEAD
-  <div class="foot_div">
-    <el-text>jh大作业@2023.10.1</el-text>
-    <hr>
-    <el-text>点击焦♂流♂前端</el-text>
-    <el-link href="https://github.com/HerveyB3B4/TeamRegistrationSystem-Front">hvのgit仓库</el-link>
-  </div>
-</template>
-
-<style scoped>
-.foot_div {
-  position: absolute;
-  left: 2%;
-  right: 2%;
-  bottom: 2%;
-}
-=======
   <el-divider content-position="center">@团队报名系统 2023.10</el-divider>
   <el-descriptions :column="3" size="small">
     <el-descriptions-item label="前端">
@@ -37,5 +20,4 @@
 
 <style scoped>
 
->>>>>>> refs/remotes/origin/main
 </style>

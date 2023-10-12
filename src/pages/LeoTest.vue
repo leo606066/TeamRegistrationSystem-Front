cfa6eb2687292d149c1e52871dca7bbd01dfc2c9
leo606066 <239599191@qq.com>
2023-10-12 09:32:52 +0800

    7

--- src/pages/LeoTest.vue
+++ src/pages/LeoTest.vue
@@ -1,10 +1,19 @@
 <template>
-    Leo
+      <div class="app">
+    <h1>搜索团队信息</h1>
+    <team-search />
+  </div>
 </template>
 
 <script setup lang="ts">
+
+import TeamSearch from '../components/Teamsearch.vue';
+
+
+
 </script>
 
 <style scoped>
+    
 </style>
   

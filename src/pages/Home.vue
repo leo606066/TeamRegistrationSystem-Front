6c13796821e454118592c3764d831e04fbf91058
HerveyB3B4 <zhwaaaaa@outlook.com>
2023-10-13 10:57:37 +0800

    home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,21 +4,13 @@
   <el-card>
     <template #header>
       <strong><span>争分夺秒吧！少年！</span></strong>
-
     </template>
-<<<<<<< HEAD
-    <ProfileVue name="艾斯比" avatar="http://47.115.209.120:8080/uploads/735a81af-9337-4d2b-80c0-dcc602bc06da.jpg" /><hr>
-    <strong>拥有一颗奔腾的心！（没有毒鸡汤你让前端怎么活！！</strong>
-=======
     <ProfileVue name="艾斯比" avatar="http://47.115.209.120:8080/uploads/674a32d1-760e-43cd-8f89-d65031649a18.jpg" /><hr>
     <strong><p class="a">拥有一颗奔腾的心！（没有毒鸡汤你让前端怎么活！！</p></strong>
->>>>>>> refs/remotes/origin/main
-
   </el-card>
   <el-card>
     <el-text><Sentences /></el-text>
   </el-card>
-
 </div>
 </template>
 
@@ -27,8 +19,6 @@
   import TimeVue from '../components/time.vue';
   import Sentences from '../components/sentences.vue';
 </script>
-
-
 
 <style scoped>
   .el-card {
